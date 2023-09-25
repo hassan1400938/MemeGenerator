37955904fc78d4e5311cd3a7072f8bfbbb3e2037
hassan1400938 <mohammadwaqarulhassan@gmail.com>
2023-09-25 18:02:07 +0500

    add header and maincontent elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import "./App.css";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import "./index.css";
 
 export default function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <MainContent />
+    </div>
+  );
 }
