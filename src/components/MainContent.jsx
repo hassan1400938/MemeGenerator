9cf60c4d5204f6d92ec2f9e726cdd691fdd3e8e9
hassan1400938 <mohammadwaqarulhassan@gmail.com>
2023-09-25 19:32:44 +0500

    css magic

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,10 +4,14 @@ export default function MainContent() {
   return (
     <main className="main">
       <div className="main--inputs">
-        <input type="text" className="main--setup" />
-        <input type="text" className="main--punchline" />
+        <input type="text" className="main--setup" placeholder="Set up line" />
+        <input
+          type="text"
+          className="main--punchline"
+          placeholder="Punch line"
+        />
       </div>
-      <button className="main--get-meme-img">Get a new meme image 🖼</button>
+      <button className="main--get-meme-img">Get a new meme image 🖼️</button>
       <div className="main--meme-img">
         <h2 className="meme-img--setup">Shut up</h2>
         <h2 className="meme-img--punchlinne">and take my money</h2>
