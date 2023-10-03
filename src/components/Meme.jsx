9b8c6ee9d2396dd54878abc3f3eb44b413b3eb7d
hassan1400938 <mohammadwaqarulhassan@gmail.com>
2023-10-03 18:02:04 +0500

    Make inputs functional and fetch data from API

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,32 +1,78 @@
 import React from "react";
-import memeData from "./memeData";
-import meme from "../assets/memeimg.png";
+// import memeData from "./memeData";
 
 export default function MainContent() {
-  const [memeImage, setMemeImage] = React.useState(`${meme}`);
-    
-  
-  
+  const [formData, setFormData] = React.useState({
+    setUpLine: "",
+    punchLine: "",
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value, // [event.target.name]: event.target.value
+    }));
+  }
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [memesData, setMemesData] = React.useState({});
+
+  // React.useEffect(() => {
+  //   fetch("https://api.imgflip.com/get_memes")
+  //     .then((res) => res.json())
+  //     .then((data) => setMemesData(data));
+  // }, []);
+
+  React.useEffect(() => {
+    async function getApiMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setMemesData(data);
+    }
+    getApiMemes();
+  }, []);
+
   function getMeme() {
-    const memeArray = memeData.data.memes;
+    const memeArray = memesData.data.memes;
     const getRendonNumber = Math.floor(Math.random() * memeArray.length);
     const url = memeArray[getRendonNumber].url;
-    setMemeImage(url);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
 
   return (
     <main className="main">
       <div className="form">
-        <input type="text" className="form--input" placeholder="Set up line" />
-        <input type="text" className="form--input" placeholder="Punch line" />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Set up line"
+          name="setUpLine"
+          onChange={handleChange}
+          value={formData.setUpLine}
+        />
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Punch line"
+          name="punchLine"
+          onChange={handleChange}
+          value={formData.punchLine}
+        />
         <button onClick={getMeme} className="form--button">
           Get a new meme image 🖼️
         </button>
       </div>
       <div className="main--meme">
-        <img src={memeImage} alt="meme-image" className="meme--img" />
-        <h2 className="meme--setup">Shut up</h2>
-        <h2 className="meme--punchline">and take my money</h2>
+        <img src={meme.randomImage} alt="meme-image" className="meme--img" />
+        <h2 className="meme--setup">{formData.setUpLine}</h2>
+        <h2 className="meme--punchline">{formData.punchLine}</h2>
       </div>
     </main>
   );
